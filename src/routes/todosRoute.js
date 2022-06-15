901d2b86580ffd9abc1ce5235a393b7682fba26e
albi-mema <amema@unitsystem.net>
2022-06-15 20:32:53 +0200

    moved schemas

--- src/routes/todosRoute.js
+++ src/routes/todosRoute.js
@@ -1,83 +1,10 @@
 let todos = require("../todos.json");
-
-
-const getTODOsOpts = {
-  schema: {
-    response: {
-      200: {
-        type: "array",
-        items:{
-          type: "object",
-          properties: {
-            id: { type: "integer" },
-            status: { type: "boolean" },
-            content: { type: "string" }
-          },
-        },
-      },
-    },
-  },
-};
-
-const getTODOOpts = {
-  schema: {
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          id: { type: "integer" },
-          status: { type: "boolean" },
-          content: { type: "string" }
-        },
-      },
-    },
-  },
-};
-
-
-const postTODOopts ={
-  schema:{
-    response:{
-      201:{
-        type: "object",
-        properties: {
-          id: { type: "integer" },
-          status: { type: "boolean" },
-          content: { type: "string" }
-        },
-      },
-    }
-  }
-}
-
-
-const deleteTODOopts ={
-  schema:{
-    response:{
-      201:{
-        type: "object",
-        properties:{ 
-          message:{ type:"string"}
-        }
-      },
-    }
-  }
-}
-
-
-
-const putTODOopts ={
-  schema:{
-    response:{
-      201:{
-        type: "object",
-        properties:{ 
-          message:{ type:"string"}
-        }
-      },
-    }
-  }
-}
+const {putTODOopts,
+       deleteTODOopts,
+       postTODOopts,
+       getTODOsOpts,
+       getTODOOpts
+      } = require("../schemas/schema")
 
 
 
---
+++ src/schemas/schema.js
@@ -0,0 +1,82 @@
+
+const getTODOsOpts = {
+    schema: {
+      response: {
+        200: {
+          type: "array",
+          items:{
+            type: "object",
+            properties: {
+              id: { type: "integer" },
+              status: { type: "boolean" },
+              content: { type: "string" }
+            },
+          },
+        },
+      },
+    },
+  };
+  
+  const getTODOOpts = {
+    schema: {
+      response: {
+        200: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            status: { type: "boolean" },
+            content: { type: "string" }
+          },
+        },
+      },
+    },
+  };
+  
+  
+  const postTODOopts ={
+    schema:{
+      response:{
+        201:{
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            status: { type: "boolean" },
+            content: { type: "string" }
+          },
+        },
+      }
+    }
+  }
+  
+  
+  const deleteTODOopts ={
+    schema:{
+      response:{
+        201:{
+          type: "object",
+          properties:{ 
+            message:{ type:"string"}
+          }
+        },
+      }
+    }
+  }
+  
+  
+  
+  const putTODOopts ={
+    schema:{
+      response:{
+        201:{
+          type: "object",
+          properties:{ 
+            message:{ type:"string"}
+          }
+        },
+      }
+    }
+  }
+  
+
+
+module.exports = {putTODOopts,deleteTODOopts,postTODOopts,getTODOsOpts,getTODOOpts}
